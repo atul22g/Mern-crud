18b005adb9495da121bc0c737c065461319dbddb
Atugaran <atul22g857086@gmail.com>
2024-01-14 19:47:59 +0530

    improve pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Products from "./components/Products";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Form from "./components/Form";
 
